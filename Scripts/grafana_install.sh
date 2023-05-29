a8c5077ff9f4e470b876aad82e16d08a2a78e39a
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-05-29 17:37:50 +0530

    added promtail and loki configs

--- Scripts/grafana_install.sh
+++ Scripts/grafana_install.sh
@@ -13,3 +13,22 @@ sudo apt-get update
 
 # Install the latest OSS release:
 sudo apt-get install grafana
+
+
+## Install Loki and Promtail using Docker
+
+# Download Loki Config - Loki is an log aggregation tool
+
+wget https://raw.githubusercontent.com/grafana/loki/v2.8.0/cmd/loki/loki-local-config.yaml -O loki-config.yaml
+
+# Run Loki Docker container
+
+docker run -d --name loki -v $(pwd):/mnt/config -p 3100:3100 grafana/loki:2.8.0 --config.file=/mnt/config/loki-config.yaml
+
+# Download Promtail Config - promtail is the agent, responsible for gathering logs and sending them to Loki
+
+wget https://raw.githubusercontent.com/grafana/loki/v2.8.0/clients/cmd/promtail/promtail-docker-config.yaml -O promtail-config.yaml
+
+# Run Promtail Docker container
+
+docker run -d --name promtail -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:2.8.0 --config.file=/mnt/config/promtail-config.yaml
