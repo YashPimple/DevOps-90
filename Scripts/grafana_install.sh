87b1fb4ce30dc61d63f01c748e0cb97d60aae479
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-05-29 11:46:13 +0530

    added grafana

---
+++ Scripts/grafana_install.sh
@@ -0,0 +1,15 @@
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+
+# Stable release
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+# Beta release
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+# Update the list of available packages
+sudo apt-get update
+
+# Install the latest OSS release:
+sudo apt-get install grafana
